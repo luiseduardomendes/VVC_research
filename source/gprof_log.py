e619248733fc3c329619a5dc8532b133c1c75f6e
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2022-11-02 22:07:11 -0300

    update

--- source/gprof_log.py
+++ source/gprof_log.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 import numpy as np
 
-def read_gprof_out(file, max_n = 15):           
+def read_gprof_out(file, max_n = 10000, specific_function : tuple = None, specific_class : tuple = None):           
 
     keys = ('time', 'cum_sec', ' self_sec', 'calls', 'self_s_call', 'tot_s_call', 'name', 'class')
 
@@ -17,6 +17,22 @@ def read_gprof_out(file, max_n = 15):
             check = pattern.findall(line)
             if len(check) != 0:
                 buffer = check[0][:]
+
+                if '::' in buffer[6]:
+                    class_name = buffer[6].split(sep='::')[0]
+                else:
+                    class_name = np.nan
+
+                funct_name = buffer[6].split(sep='(')[0]
+                funct_name = funct_name.split(sep='::')[-1]
+                
+                if specific_class != None:
+                    if class_name not in specific_class:
+                        continue
+                if specific_function != None:
+                    if funct_name not in specific_function:
+                        continue
+
                 for i, key in enumerate(keys):
                     if buffer[i] == '':
                         structBuffer[key].append(np.nan)
@@ -25,13 +41,7 @@ def read_gprof_out(file, max_n = 15):
                             structBuffer[key].append(float(buffer[i]))
                         except:
                             structBuffer[key].append((buffer[i]))
-                if '::' in buffer[6]:
-                    class_name = buffer[6].split(sep='::')[0]
-                else:
-                    class_name = np.nan
-
-                funct_name = buffer[6].split(sep='(')[0]
-                funct_name = funct_name.split(sep='::')[-1]
+                
                 structBuffer['class'][-1] = class_name
                 structBuffer['name'][-1] = funct_name
 
