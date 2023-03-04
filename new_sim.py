14114c23dc4e1eae6378e11815a3dd75a61c68b0
luiseduardomendes <dudupemendes13@gmail.com>
2023-03-04 10:39:11 -0300

    update

---
+++ new_sim.py
@@ -0,0 +1,6 @@
+import source.vvc_simulation as vs
+
+s = vs.Simulation(n_frames=8, encoder=['RA'], bg_exec=True)
+s.set_vtm_dir('home/luispmendes/VVCSoftware_VTM/')
+s.set_cfg_dir('home/luispmendes/VVCSoftware_VTM//)
+
