943c5b5d38a5d87e96018da00af3b844d121863c
luiseduardomendes <dudupemendes13@gmail.com>
2023-05-04 11:30:33 -0300

    update

--- new_sim.py
+++ new_sim.py
@@ -2,11 +2,9 @@ import source.vvc_simulation as vs
 
 vtm_dir = '/home/luispmendes/VVCSoftware_VTM/'
 cfg_dir = '/home/luispmendes/VVCSoftware_VTM/cfg-files-test/'
-out_dir = '/home/luispmendes/VVC_research/output030523/'
+out_dir = '/home/luispmendes/VVC_research/output040523-1/'
 
-
-
-sim = vs.Simulation(n_frames=32, encoder=['RA'], qps=[37], bg_exec=True, gprof=True)
+sim = vs.Simulation(n_frames=32, encoder=['RA', 'AI'], qps=[37], bg_exec=True, gprof=True)
 sim.set_paths(out_dir, vtm_dir, cfg_dir)
 
 sim.run_exec()
