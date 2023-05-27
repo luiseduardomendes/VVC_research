2430786f4def70d519a6357fdeb774dfcbdcbeb0
luiseduardomendes <dudupemendes13@gmail.com>
2023-05-27 16:19:11 -0300

    update

--- implement_gprof.py
+++ implement_gprof.py
@@ -1,4 +1,9 @@
 import source.common.commonlib as c
+import source.vvc_simulation as sim
 
+old_file = "/home/luispmendes/VVCSoftware_VTM/src/Lib/CommonLib/"
+new_file = "/home/luispmendes/VVC_research/RdCostModif.cpp"
+
+c.file_subs(new_file, old_file, 'RdCost.cpp')
 c.compile_VTM('/home/luispmendes/VVCSoftware_VTM')
 
--- new_sim.py
+++ new_sim.py
@@ -2,9 +2,9 @@ import source.vvc_simulation as vs
 
 vtm_dir = '/home/luispmendes/VVCSoftware_VTM/'
 cfg_dir = '/home/luispmendes/VVCSoftware_VTM/cfg-files-test/'
-out_dir = '/home/luispmendes/VVC_research/output/'
+out_dir = '/home/luispmendes/VVC_research/output-3/'
 
-sim = vs.Simulation(n_frames=32, encoder=['RA', 'AI'], qps=[22, 27, 32, 37], bg_exec=True, gprof=True)
+sim = vs.Simulation(n_frames=8, encoder=['RA'], qps=[22, 27, 32, 37], bg_exec=True, gprof=True)
 sim.set_paths(out_dir, vtm_dir, cfg_dir)
 
 sim.run_exec()
